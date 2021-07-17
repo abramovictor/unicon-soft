f2912c4ff4b17ac40f7fb0100aa49acb979dc90e
adaltezze <adaltezze@gmail.com>
2021-07-17 05:12:19 +0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,7 @@ assign(styles, {
 
 const scripts = () => pipeline(
   gulp.src([
-    // './node_modules/bootstrap/js/dist/'
+    './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
     './node_modules/focus-visible/dist/focus-visible.js',
     paths.scripts.src,
   ]),
