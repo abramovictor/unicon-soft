91561660bf783b8cef4e91198898551475808b1a
adaltezze <adaltezze@gmail.com>
2021-07-24 11:03:05 +0500

    update

--- src/js/home-counters.js
+++ src/js/home-counters.js
@@ -1,22 +1,27 @@
 (() => {
-  const countersSection = document.querySelector('#performance-in-numbers');
-  const countersElements = countersSection.querySelectorAll('[data-counter-value]');
+  const countersSection = document.querySelectorAll('[data-reveal]');
+  const countersElements = document.querySelectorAll('[data-counter-value]');
 
-  const initializeCounters = () => {
-    countersElements.forEach(counterElement => {
-      const counterEndValue = counterElement.getAttribute('data-counter-value');
-      const countUpObject = new countUp.CountUp(counterElement, counterEndValue, { separator: ' ' });
+  const initializeCounters = element => {
+    if (element.getAttribute('data-animation') === 'countup') {
+      countersElements.forEach(counterElement => {
+        const counterEndValue = counterElement.getAttribute('data-counter-value');
+        const countUpObject = new countUp.CountUp(counterElement, counterEndValue, { separator: ' ' });
 
-      if (!countUpObject.error) {
-        countUpObject.start();
-      } else {
-        console.error(countUpObject.error);
-      }
-    });
+        if (!countUpObject.error) {
+          countUpObject.start();
+        } else {
+          console.error(countUpObject.error);
+        }
+      });
+    }
   };
 
   ScrollReveal().reveal(countersSection, {
-    viewFactor: 0.7,
+    viewFactor: 0.25,
     afterReveal: initializeCounters,
+    distance: '100px',
+    scale: 0.9,
+    duration: 800,
   });
 })();
